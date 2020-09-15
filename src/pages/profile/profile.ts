ed2862257e3d00ce84c8538b42f0d39137dc0810
ramilani12 <ramilani@gmail.com>
2020-09-14 22:45:16 -0300

    Tirando foto

--- src/pages/profile/profile.ts
+++ src/pages/profile/profile.ts
@@ -4,6 +4,7 @@ import { StorageService } from '../../services/storage_service';
 import { ClienteService } from '../../services/domain/cliente.service';
 import { ClienteDTO } from '../../models/cliente.dto';
 import { API_CONFIG } from '../../config/api.config';
+import { CameraOptions , Camera} from '@ionic-native/camera';
 
 @IonicPage()
 @Component({
@@ -13,8 +14,11 @@ import { API_CONFIG } from '../../config/api.config';
 export class ProfilePage {
 
   cliente : ClienteDTO;
+  picture: string;
+  cameraOn: boolean = false;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams , public storage : StorageService, public clienteService : ClienteService) {
+  constructor(public navCtrl: NavController, public navParams: NavParams , public storage : StorageService, 
+    public clienteService : ClienteService,  public camera: Camera) {
   }
 
   ionViewDidLoad() {
@@ -46,6 +50,24 @@ export class ProfilePage {
       error => {
           console.error(error);
       });
+  }
+
+  getCameraPicture() {
+
+    this.cameraOn = true;
+
+    const options: CameraOptions = {
+      quality: 100,
+      destinationType: this.camera.DestinationType.DATA_URL,
+      encodingType: this.camera.EncodingType.PNG,
+      mediaType: this.camera.MediaType.PICTURE
+    }
+
+    this.camera.getPicture(options).then((imageData) => {
+     this.picture = 'data:image/png;base64,' + imageData;
+     this.cameraOn = false;
+    }, (err) => {
+    });
   }
 
 }
