7eba54e68e5cc149ca46a34cffcd3826172a8e71
ramilani12 <ramilani@gmail.com>
2020-11-22 15:54:43 -0300

    Bugfix ajuste foto profile

--- src/pages/profile/profile.ts
+++ src/pages/profile/profile.ts
@@ -5,6 +5,7 @@ import { ClienteService } from '../../services/domain/cliente.service';
 import { ClienteDTO } from '../../models/cliente.dto';
 import { API_CONFIG } from '../../config/api.config';
 import { CameraOptions , Camera} from '@ionic-native/camera';
+import { DomSanitizer } from '@angular/platform-browser';
 
 @IonicPage()
 @Component({
@@ -16,9 +17,12 @@ export class ProfilePage {
   cliente : ClienteDTO;
   picture: string;
   cameraOn: boolean = false;
+  profileImage;
 
   constructor(public navCtrl: NavController, public navParams: NavParams , public storage : StorageService, 
-    public clienteService : ClienteService,  public camera: Camera) {
+    public clienteService : ClienteService,  public camera: Camera, public sanitizer: DomSanitizer) {
+
+      this.profileImage = 'assets/imgs/avatar-blank.png';
   }
 
   ionViewDidLoad() {
@@ -48,11 +52,25 @@ export class ProfilePage {
 
   }
 
+   // https://gist.github.com/frumbert/3bf7a68ffa2ba59061bdcfc016add9ee
+   blobToDataURL(blob) {
+    return new Promise((fulfill, reject) => {
+        let reader = new FileReader();
+        reader.onerror = reject;
+        reader.onload = (e) => fulfill(reader.result);
+        reader.readAsDataURL(blob);
+    })
+  }
+
   getImageIfExists() {
     this.clienteService.getImageFromBucket(this.cliente.id)
       .subscribe(response => {
 
           this.cliente.imageUrl = `${API_CONFIG.bucketBaseUrl}/cp${this.cliente.id}.jpg`
+          this.blobToDataURL(response).then(dataUrl => {
+            let str : string = dataUrl as string;
+            this.profileImage = this.sanitizer.bypassSecurityTrustUrl(str);
+          });
       },
       error => {
           console.error(error);
@@ -110,7 +128,7 @@ export class ProfilePage {
     .subscribe(response=> {
 
       this.picture = null;
-      this.loadData();
+      this.getImageIfExists();
 
     }, error => {
 
